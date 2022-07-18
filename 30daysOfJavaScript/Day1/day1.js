c9016350ada4f7c41297c656b8a894c239c27fc5
Kaito1820 <nguyenduykhanh1820@gmail.com>
2022-07-18 16:39:09 +0700

    Day 1

---
+++ 30daysOfJavaScript/Day1/day1.js
@@ -0,0 +1,10 @@
+console.log('buh buh lmao lmao')
+
+let firstName
+console.log(firstName) //not defined, because it is not assigned to a value yet
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
