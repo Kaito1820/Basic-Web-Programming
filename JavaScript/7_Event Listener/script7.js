37c4fcbee4bffe533d862e8b5590813da2273050
Kaito1820 <nguyenduykhanh1820@gmail.com>
2022-08-01 10:57:44 +0700

    Added example of removeEventListener

--- JavaScript/7_Event Listener/script7.js
+++ JavaScript/7_Event Listener/script7.js
@@ -1,5 +1,19 @@
 let btn = document.querySelector('.btn');
 
-btn.addEventListener('click', function(e){
-    console.log('Hello');
-});
+function viec1(){
+    console.log('bủh bủh')
+}
+function viec2(){
+    console.log('lmao lmao')
+}
+
+btn.addEventListener('click', viec1)
+btn.addEventListener('click', viec2)
+
+// btn.addEventListener('click', function(e){
+//     console.log('Hello');
+// });
+
+setTimeout(function(){
+    btn.removeEventListener('click', viec1)
+}, 3000);
