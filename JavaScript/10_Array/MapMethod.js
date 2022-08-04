1921fa4e85463b015c874c195d18781acfe18907
Kaito1820 <nguyenduykhanh1820@gmail.com>
2022-08-04 10:19:25 +0700

    Compare forEach and Map method

--- JavaScript/10_Array/MapMethod.js
+++ JavaScript/10_Array/MapMethod.js
@@ -40,3 +40,11 @@ function courseHandler(course,index, originArray){
 let newCourse = courses.map(courseHandler)// map nhu 1 vong lap, lap qua tung phan tu roi goi ham ben trong de thuc thi
 
 console.log(newCourse)
+
+/*
+The main difference between map and forEach is that the map method returns a new array 
+by applying the callback function on each element of an array, 
+while the forEach method doesn't return anything. 
+You can use the forEach method to mutate the source array, 
+but this isn't really the way it's meant to be used.
+*/
